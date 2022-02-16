584a3f18d60571de686be908f44a7cb5ef4adfe7
albertoluian <alberto.luian@gmail.com>
2022-02-16 18:22:32 -0300

    cors added

--- crud/src/server.js
+++ crud/src/server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const routes = require('./routes.js');
 const morgan = require('morgan');
+const cors = require('cors');
 require('dotenv/config');
 const port = process.env.PORTA;
 require('./database');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors());
 app.use(morgan('dev'));
 app.use(routes);
 
