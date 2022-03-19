9d85a2cc04a03a7522bacd69fe6d1b24bac25370
albertoluian <alberto.luian@gmail.com>
2022-03-19 18:26:56 -0300

    heroku config

--- crud/src/config/database.js
+++ crud/src/config/database.js
@@ -5,6 +5,7 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_DATABASE,
+    ssl: true,
     dialectOptions:{
         ssl: {
             rejectUnauthorized: false 
