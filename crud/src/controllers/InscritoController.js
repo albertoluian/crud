5b5ac5f425d2f994213fced06658911fe048ce31
albertoluian <alberto.luian@gmail.com>
2022-04-24 18:58:40 -0300

    clodinary tests

--- crud/src/controllers/InscritoController.js
+++ crud/src/controllers/InscritoController.js
@@ -3,23 +3,25 @@ const Admin = require('../models/Admin');
 const bcrypt = require('bcryptjs');
 const { Sequelize } = require('sequelize');
 const jwt = require('jsonwebtoken');
+const Formidable = require('formidable');
+const cloudinary = require("cloudinary");
 require('dotenv/config');
 const SECRET = process.env.SEGREDO;
 const validar = require('./testaCPF.js');
-function verificarToken(token, id){
+function verificarToken(token, id) {
   var resultado = true;
-  jwt.verify(token, SECRET,(err, decoded)=>{
-    if(!err)
-    resultado = true;
+  jwt.verify(token, SECRET, (err, decoded) => {
+    if (!err)
+      resultado = true;
     else
-    resultado = false;
+      resultado = false;
   });
-    return resultado;
- }
+  return resultado;
+}
 module.exports = {
-   async store(req, res, next){
-     try{
-       const {
+  async store(req, res, next) {
+    try {
+      const {
         nome,
         sexo,
         nomeMae,
@@ -38,7 +40,7 @@ module.exports = {
         cep,
         cidade,
         telefoneResidencial,
-        telefone,       
+        telefone,
         email1,
         email2,
         cursoDeGraduacao1,
@@ -76,14 +78,29 @@ module.exports = {
         documentosComprobatorios,
         reservista,
         vinculoUece,
-        } =  JSON.parse(req.body.dados);
-       const inscritos = await Inscrito.findOne({where:Sequelize.or(
-         {email1:email1},{cpf:cpf},{telefone:telefone},{rg:rg},
-         )
-        }).catch(err => { throw new Error (err)});
-       if(inscritos)
-         return res.status(409).json({error: "Dados ja cadastrados"});
-       const inscrito = await Inscrito.create({
+      } = JSON.parse(req.body.dados);
+      
+      cloudinary.config({
+        cloud_name: process.env.CLOUD_NAME,
+        api_key: process.env.API_KEY,
+        api_secret: process.env.API_SECRET
+    });
+    cloudinary.uploader
+      .upload(req.files[0].path)
+      .then((result) => {
+        console.log(result);
+        res.status(200).json({
+          message: "success",
+          result,
+        })}).catch(err =>{throw new Error(err) })
+      const inscritos = await Inscrito.findOne({
+        where: Sequelize.or(
+          { email1: email1 }, { cpf: cpf }, { telefone: telefone }, { rg: rg },
+        )
+      }).catch(err => { throw new Error(err) });
+      if (inscritos)
+        return res.status(409).json({ error: "Dados ja cadastrados" });
+      const inscrito = await Inscrito.create({
         nome,
         sexo,
         nomeMae,
@@ -102,7 +119,7 @@ module.exports = {
         cep,
         cidade,
         telefoneResidencial,
-        telefone,       
+        telefone,
         email1,
         email2,
         cursoDeGraduacao1,
@@ -140,59 +157,80 @@ module.exports = {
         documentosComprobatorios,
         reservista,
         vinculoUece
-        })
-        .catch(err => { throw new Error (err)});
-     return res.json("Inscrito com sucesso!!!");
-       
-      
-   }
-  catch(e){
-    console.log(e);
-    next(e);
-  }
-  },
-  async getAll(req, res, next){
-    try{
-    const token = req.headers['authorization'];
-    const verif = verificarToken(token);
-    if(verif){
-    const admin = await Admin.findOne({where:{token:token}}).catch(err => { throw new Error (err)});
-    if(admin){
-    const inscritos = await Inscrito.findAll().catch(err => { throw new Error (err)});
-    return res.json(inscritos);
-    }
-    else 
-    return res.status(403).json("Token invalido");
-  }
-    else
-    return res.status(403).json("Token invalido");
-  }
-  catch(e){
-    console.log(e);
-    next(e);
-  }},
-async aprove(req, res, next){
-  try{
-  const {id} = req.params;
-  const token = req.headers['authorization'];
-  const verif = verificarToken(token);
-  const admin = await Admin.findOne({where:{token:token}}).catch(err => { throw new Error (err)});
-  if(verif && admin){
-    const inscritos1 = await Inscrito.findOne({where:{id:id}}).catch(err => { throw new Error (err)});
-  if(inscritos1){
-  const inscritos = await Inscrito.update({aprovado:true},{where:{id:id}}).catch(err => { throw new Error (err)});
-  return res.json(inscritos);
-}
-else
-return res.status(404).json("nao existe inscrito com tal id");
-}
-  else
-  return res.status(403).json("Token invalido");
-}
-catch(e){
-  console.log(e);
-  next(e);
-}
-}
+      })
+        .catch(err => { throw new Error(err) });
+      return res.json("Inscrito com sucesso!!!");
 
+
+    }
+    catch (e) {
+      console.log(e);
+      next(e);
+    }
+  },
+  async getAll(req, res, next) {
+    try {
+      const token = req.headers['authorization'];
+      const verif = verificarToken(token);
+      if (verif) {
+        const admin = await Admin.findOne({ where: { token: token } }).catch(err => { throw new Error(err) });
+        if (admin) {
+          const inscritos = await Inscrito.findAll().catch(err => { throw new Error(err) });
+          return res.json(inscritos);
+        }
+        else
+          return res.status(403).json("Token invalido");
+      }
+      else
+        return res.status(403).json("Token invalido");
+    }
+    catch (e) {
+      console.log(e);
+      next(e);
+    }
+  },
+  async aprove(req, res, next) {
+    try {
+      const { id } = req.params;
+      const token = req.headers['authorization'];
+      const verif = verificarToken(token);
+      const admin = await Admin.findOne({ where: { token: token } }).catch(err => { throw new Error(err) });
+      if (verif && admin) {
+        const inscritos1 = await Inscrito.findOne({ where: { id: id } }).catch(err => { throw new Error(err) });
+        if (inscritos1) {
+          const inscritos = await Inscrito.update({ aprovado: true }, { where: { id: id } }).catch(err => { throw new Error(err) });
+          return res.json(inscritos);
+        }
+        else
+          return res.status(404).json("nao existe inscrito com tal id");
+      }
+      else
+        return res.status(403).json("Token invalido");
+    }
+    catch (e) {
+      console.log(e);
+      next(e);
+    }
+  },
+  async upload(req, res, next) {
+   
+    try {
+      cloudinary.config({
+        cloud_name: process.env.CLOUD_NAME,
+        api_key: process.env.API_KEY,
+        api_secret: process.env.API_SECRET
+    });
+    cloudinary.uploader
+      .upload(req.body.files)
+      .then((result) => {
+        console.log(result);
+        res.status(200).json({
+          message: "success",
+          result,
+        })}).catch(err =>{throw new Error(err) })
+    } catch (e) {
+      console.log(e);
+      next(e);
+    }
+  }
 }
