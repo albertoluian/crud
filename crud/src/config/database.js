34a1e8e25d0a45afd52ca9e75ea61530ad80b04a
albertoluian <alberto.luian@gmail.com>
2022-06-21 12:35:16 -0300

    conditional ssl

--- crud/src/config/database.js
+++ crud/src/config/database.js
@@ -1,19 +1,26 @@
 require('dotenv/config');
+process.env.PORT ?
 module.exports = {
     dialect: process.env.DIALECT,
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_DATABASE,
-    ssl: true,
-    dialectOptions:{
-        ssl: {
-            rejectUnauthorized: false 
-      },
-    }, 
+    define:{
+        timestamps: true,
+        underscored: true,
+    },
+   }
+   :
+   module.exports = {
+    dialect: process.env.DIALECT,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
     define:{
         timestamps: true,
         underscored: true,
         
     },
-   };
+   }
